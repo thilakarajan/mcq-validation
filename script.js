d5a10eeaee1bcc80f3777b8b3d24c33c1bf13a0a
Thilakarajan <thilakarajanv29@gmail.com>
2024-07-22 16:46:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function red(element){
+    element.parentNode.style.backgroundColor = "red";
+    element.style.backgroundColor = "red";
+    element.parentNode.style.color = "white";
+  element.style.borderColor = 'red'
+  element.style.color = 'white'
+}
+
+function green(element) {
+  element.parentNode.style.backgroundColor = 'green'
+  element.style.backgroundColor = 'green'
+  element.parentNode.style.color = 'white'
+  element.style.color = 'white'
+  element.style.borderColor="green"
+}
+
